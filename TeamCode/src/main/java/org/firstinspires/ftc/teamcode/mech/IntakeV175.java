a19b3dd727a1f79c824eb7b9c3172303d563b58f
9848GearView <9848@student.clearviewregional.edu>
2026-01-30 12:36:48 -0500

    added beginnings of 2.0, LL code begun

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/IntakeV175.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/IntakeV175.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.mech;
 //
-
+//
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
